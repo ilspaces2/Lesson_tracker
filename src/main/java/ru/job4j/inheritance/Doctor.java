772e6f02a2efb5fb416f3ee791e4d8ab920fa121
I_M <moskva.88@mail.ru>
2022-01-22 14:36:12 +0300

    3.2 Реализация профессий в коде.

--- src/main/java/ru/job4j/inheritance/Doctor.java
+++ src/main/java/ru/job4j/inheritance/Doctor.java
@@ -2,8 +2,14 @@ package ru.job4j.inheritance;
 
 public class Doctor extends Profession {
 
+    private String fieldOfActivity = "Heal people";
+
     public Doctor(String name, String surname, String education, String birthday) {
         super(name, surname, education, birthday);
+    }
+
+    public String getFieldOfActivity() {
+        return fieldOfActivity;
     }
 
     public DiagnosticCard diagnostic(Pacient pacient) {
--- src/main/java/ru/job4j/inheritance/Engineer.java
+++ src/main/java/ru/job4j/inheritance/Engineer.java
@@ -2,8 +2,14 @@ package ru.job4j.inheritance;
 
 public class Engineer extends Profession {
 
+    private String engineerIt = "Engineers are called inventors";
+
     public Engineer(String name, String surname, String education, String birthday) {
         super(name, surname, education, birthday);
+    }
+
+    public String getEngineerIt() {
+        return engineerIt;
     }
 
     public DevelopmentPlan creating(int periodOfExecution, TechnicalTask technicalTask) {
