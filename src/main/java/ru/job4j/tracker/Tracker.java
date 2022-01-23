ac38b7ed601f2999da6ae7a16e3ddb46cbe2ddb4
I_M <moskva.88@mail.ru>
2022-01-23 12:45:27 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,21 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rzl = new Item[size];
+        int rzlIndex = 0;
+        for (int i = 0; i < size; i++) {
+            if (key.equals(items[i].getName())) {
+                rzl[rzlIndex++] = items[i];
+            }
+        }
+        rzl = Arrays.copyOf(rzl, rzlIndex);
+        return rzl;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
     }
 }
