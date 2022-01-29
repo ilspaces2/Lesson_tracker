a20019c3976ac1228922358eced6e6e21ac11115
I_M <moskva.88@mail.ru>
2022-01-29 12:00:45 +0300

    0.5. Иерархия исключений и множественный catch. [#219368] add break.

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -7,6 +7,7 @@ public class UserStore {
         for (User user : users) {
             if (login.equals(user.getUsername())) {
                 findUser = user;
+                break;
             }
         }
         if (findUser == null) {
