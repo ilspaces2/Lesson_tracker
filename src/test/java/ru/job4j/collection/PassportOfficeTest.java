b8a145540a38f1d79923a2bb2ffb5385f26cad63
I_M <moskva.88@mail.ru>
2022-02-02 15:47:34 +0300

    1. Паспорт и жители [#212656]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void add() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void whenAddDuplicatePassport() {
+        Citizen citizenOne = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizenTwo = new Citizen("2f44a", "Petr Petrov");
+        PassportOffice office = new PassportOffice();
+        office.add(citizenOne);
+        assertFalse(office.add(citizenTwo));
+    }
+}
