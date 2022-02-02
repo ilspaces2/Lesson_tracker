4351c40ed7decf02dc45535d49cb72f1e958622f
I_M <moskva.88@mail.ru>
2022-02-02 11:18:00 +0300

    0. Map, HashMap [#212652 #259100] add duplicate str

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,7 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("1@1.ru", "Ivan Ivanov");
+        map.put("1@1.ru", "Ivan Ivanov");
         map.put("2@2.ru", "Petr Petrov");
+        map.put("3@3.ru", "Artem Sidorov");
         map.put("3@3.ru", "Artem Sidorov");
         for (String key : map.keySet()) {
             System.out.println(key + " " + map.get(key));
