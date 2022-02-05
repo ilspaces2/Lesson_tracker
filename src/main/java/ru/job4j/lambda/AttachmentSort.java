7e83df659e84ec2fc7bc278dd69b488c15bf8ea7
I_M <moskva.88@mail.ru>
2022-02-05 20:23:09 +0300

    2.1. Лямбда [#58165]

--- src/main/java/ru/job4j/lambda/AttachmentSort.java
+++ src/main/java/ru/job4j/lambda/AttachmentSort.java
@@ -30,5 +30,14 @@ public class AttachmentSort {
         attachments.sort(cmpName);
         System.out.println(attachments);
 
+        /**
+         * Анонимные классы которые мы создали ранее можно переписать в лямбда выражение
+         */
+        Comparator<Attachment> cmpText = (left, right) -> right.getName().compareTo(left.getName());
+        Comparator<Attachment> cmpDescSize = (left, right) -> Integer.compare(right.getSize(), left.getSize());
+        attachments.sort(cmpDescSize);
+        System.out.println(attachments);
+        attachments.sort(cmpText);
+        System.out.println(attachments);
     }
 }
